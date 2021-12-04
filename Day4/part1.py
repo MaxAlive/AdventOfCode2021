d9396510cbe72ed19d58321cde939f8c53eb118b
Max <maximilian.franschitz@gmx.at>
2021-12-04 19:35:21 +0100

    Day4 Solutions and Input

---
+++ Day4/part1.py
@@ -0,0 +1,64 @@
+class BingoBoard:
+    
+    def __init__(self, input_list):
+        self.grid = input_list
+
+    def check_win(self):
+        for i in range(5):
+            if self.check_col(i) or self.check_row(i):
+                return True
+        return False
+
+    def sum_unmarked(self):
+        return sum([value for value in self.grid if value != 'x'])
+
+    def check_col(self, col):
+        for i in range(col, 25, 5):
+            if self.grid[i] != 'x':
+                return False
+        return True
+
+    def check_row(self, row):
+        for value in self.grid[row*5:row*5+5]:
+            if value != 'x':
+                return False
+        return True
+
+    def check_number(self, number):
+        try:
+            self.grid[self.grid.index(number)] = 'x'
+        except:
+            return
+
+
+with open('Day4/input.txt') as f:
+    data = [value.rstrip() for value in f.readlines()]
+
+random_numbers = [int(d) for d in data[0].split(',')]
+bingo_boards = []
+
+for d in data[1:]:
+    if d == "":
+        bingo_boards.append([])
+    else:
+        tmp_values = [int(v) for v in d.split()]
+        bingo_boards[-1].extend(tmp_values)
+
+
+bingo_boards = [BingoBoard(board) for board in bingo_boards]
+winning_board = None
+winning_number = None
+
+for number in random_numbers:
+    for board in bingo_boards:
+        board.check_number(number)
+        if board.check_win():
+            winning_board = board
+            winning_number = number
+            break
+    if winning_board:
+        break
+
+print(winning_board.grid)
+print(winning_number)
+print(winning_board.sum_unmarked() * winning_number)
